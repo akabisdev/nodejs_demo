eafdfb62ae6c90f19cb618ff7ea119f94168f153
Akash Bisht <akash.b@gaeaglobal.com>
2022-09-27 15:36:07 +0530

    Pagination added

--- routes/users.js
+++ routes/users.js
@@ -12,16 +12,25 @@ router.get('/', function (req, res, next) {
 // This section will help you get a list of all the records.
 router.route('/customers').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
+  const collection = dbConnect.collection('customers');
+  const count = await collection.count();
+  const limit = _req.query.limit ?? 2;
+  ///default value for pageNumber should be 1
+  const skip = _req.query.pageNumber > 1 ? (((_req.query.pageNumber - 1) * limit)) : 0;
 
-  dbConnect
-    .collection('customers')
+  collection
     .find({})
-    .limit(50)
+    .sort({ name: 1 })
+    .skip(skip)
+    .limit(parseInt(limit))
     .toArray(function (err, result) {
       if (err) {
         res.status(400).send('Error fetching listings!');
       } else {
-        res.json(result);
+        res.json({
+          message: 'Success', data: result,
+          totalCount: count,
+        });
       }
     });
 });
@@ -36,12 +45,15 @@ router.route('/customers').post(function (req, res) {
 
   dbConnect
     .collection('customers')
-    .insertOne(matchDocument, function (err, result) {
+    .insertOne(req.body, function (err, result) {
       if (err) {
         res.status(400).send('Error inserting matches!');
       } else {
         console.log(`Added a new match with id ${result.insertedId}`);
-        res.status(201).send(matchDocument);
+        res.status(201).send({
+          message: 'Successfully created',
+          data: result,
+        });
       }
     });
 });
